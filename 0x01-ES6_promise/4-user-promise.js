3b423eb9a0ffe766b044f07e5815735276a4dfab
BallansBytes <maballans@gmail.com>
2024-07-23 13:48:14 -0700

    Correction to 4-user-promises task 4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,16 +1,3 @@
-#!/usr/bin/node
-
-/**
- * signUpUser - Function to return a resolved promise with user details
- * @param {string} firstName - The first name of the user
- * @param {string} lastName - The last name of the user
- * @returns {Promise} - A promise that resolves to an object with firstName and lastName
- */
-export function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName: firstName,
-    lastName: lastName,
-  });
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
 }
-
-module.exports = signUpUser;
