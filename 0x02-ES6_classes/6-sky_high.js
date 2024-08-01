3f8abb1d325f3551b5219dd79682714eaae2521d
BallansBytes <maballans@gmail.com>
2024-08-01 08:04:23 -0700

    Task 6 amendment

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,20 +1,20 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-constructor(sqft, floors) {
-  super(sqft);
+  constructor(sqft, floors) {
+    super(sqft);
     this.floors = floors;
-    }
+  }
 
-get floors() {
-  return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-set floors(value) {
-  this._floors = value;
-    }
-  
-evacuationWarningMessage() {
-  return `Evacuate slowly the ${this.floors} floors`;
-    }
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
   }
