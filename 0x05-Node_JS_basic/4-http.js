1cd23787d35f93f2648fcd788aca945709a6f761
Ballansbyte <maballans@gmail.com>
2024-09-24 13:12:40 +0000

    Tasks

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,7 +4,6 @@ const PORT = 1245;
 const HOST = 'localhost';
 const app = http.createServer();
 
-
 app.on('request', (_, res) => {
   const responseText = 'Hello Holberton School!';
 
@@ -17,5 +16,4 @@ app.on('request', (_, res) => {
 app.listen(PORT, HOST, () => {
   process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
-
 module.exports = app;
